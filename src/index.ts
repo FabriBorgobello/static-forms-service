76d0ecd670598b6b40522010aead7faca6e9995e
Fabricio Borgobello <fborgobello@gmail.com>
2023-11-27 23:02:54 +0100

    Add middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,36 @@
 import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+
+import formsRouter from './routes/forms';
+
 const app = express();
 const port = process.env.PORT ?? 3000;
 
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+// Routes
+app.use('/api/forms', formsRouter);
+
+// Not Found Handler
+app.use((req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404).json({ message: error.message });
+  next(error);
+});
+
+// Health Check
+app.get('/health', (_, res) => {
+  res.status(200).json({ ok: true });
+});
 
 app.get('*', (_, res) => {
   res.redirect('/');
