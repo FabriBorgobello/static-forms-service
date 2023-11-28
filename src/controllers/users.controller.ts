6410f20e7594e56953ba456836d3788bae9dcc8a
Fabricio Borgobello <fborgobello@gmail.com>
2023-11-28 16:35:38 +0100

    Add bcrypt

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,11 +1,14 @@
 import { db } from '@/database';
 import { NotFoundError } from '@/utils/error-handler';
 import { NextFunction, Request, Response } from 'express';
+import bcrypt from 'bcrypt';
+
+const publicFields = ['id', 'name', 'email', 'created_at', 'updated_at'];
 
 // List
 export const getUsers = async (_req: Request, res: Response, next: NextFunction) => {
   try {
-    const users = await db('users').select('*');
+    const users = await db('users').select(publicFields);
     res.json(users);
   } catch (error) {
     next(error);
@@ -16,7 +19,7 @@ export const getUsers = async (_req: Request, res: Response, next: NextFunction)
 export const getUserById = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { id } = req.params;
-    const user = await db('users').where({ id }).first();
+    const user = await db('users').where({ id }).first().select(publicFields);
     if (!user) throw new NotFoundError();
     res.json(user);
   } catch (error) {
@@ -28,7 +31,8 @@ export const getUserById = async (req: Request, res: Response, next: NextFunctio
 export const createUser = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { name, email, password } = req.body;
-    const [user] = await db('users').insert({ name, email, password }).returning('*');
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const [user] = await db('users').insert({ name, email, password: hashedPassword }).returning(publicFields);
     res.status(201).json(user);
   } catch (error) {
     next(error);
@@ -40,7 +44,8 @@ export const updateUser = async (req: Request, res: Response, next: NextFunction
   try {
     const { id } = req.params;
     const { name, email, password } = req.body;
-    const user = await db('users').where({ id }).update({ name, email, password }).returning('*');
+    const [user] = await db('users').where({ id }).update({ name, email, password }).returning(publicFields);
+    if (!user) throw new NotFoundError();
     res.json(user);
   } catch (error) {
     next(error);
@@ -51,7 +56,8 @@ export const updateUser = async (req: Request, res: Response, next: NextFunction
 export const deleteUser = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { id } = req.params;
-    await db('users').where({ id }).delete();
+    const user = await db('users').where({ id }).delete();
+    if (!user) throw new NotFoundError();
     res.status(204).send();
   } catch (error) {
     next(error);
--- src/database/seeds/01_initial_users.ts
+++ src/database/seeds/01_initial_users.ts
@@ -1,4 +1,5 @@
 import { Knex } from 'knex';
+import bcrypt from 'bcrypt';
 
 export async function seed(knex: Knex): Promise<void> {
   // Deletes ALL existing entries
@@ -6,15 +7,15 @@ export async function seed(knex: Knex): Promise<void> {
 
   // Inserts seed entries
   await knex('users').insert([
-    { name: 'John Doe', password: 'johndoe', email: 'johndoe@email.com' },
-    { name: 'Jane Doe', password: 'janedoe', email: 'janedoe@email.com' },
-    { name: 'John Smith', password: 'johnsmith', email: 'johnsmith@email.com' },
-    { name: 'Alice Johnson', password: 'alicejohnson', email: 'alicej@email.com' },
-    { name: 'Bob Brown', password: 'bobbrown', email: 'bobb@email.com' },
-    { name: 'Charlie Davis', password: 'charliedavis', email: 'charlied@email.com' },
-    { name: 'Diana Evans', password: 'dianaevans', email: 'dianae@email.com' },
-    { name: 'Ethan Fox', password: 'ethanfox', email: 'ethanf@email.com' },
-    { name: 'Fiona Green', password: 'fionagreen', email: 'fionag@email.com' },
-    { name: 'George Harris', password: 'georgeharris', email: 'georgeh@email.com' },
+    { name: 'John Doe', password: bcrypt.hash('johndoe', 10), email: 'johndoe@email.com' },
+    { name: 'Jane Doe', password: bcrypt.hash('janedoe', 10), email: 'janedoe@email.com' },
+    { name: 'John Smith', password: bcrypt.hash('johnsmith', 10), email: 'johnsmith@email.com' },
+    { name: 'Alice Johnson', password: bcrypt.hash('alicejohnson', 10), email: 'alicej@email.com' },
+    { name: 'Bob Brown', password: bcrypt.hash('bobbrown', 10), email: 'bobb@email.com' },
+    { name: 'Charlie Davis', password: bcrypt.hash('charliedavis', 10), email: 'charlied@email.com' },
+    { name: 'Diana Evans', password: bcrypt.hash('dianaevans', 10), email: 'dianae@email.com' },
+    { name: 'Ethan Fox', password: bcrypt.hash('ethanfox', 10), email: 'ethanf@email.com' },
+    { name: 'Fiona Green', password: bcrypt.hash('fionagreen', 10), email: 'fionag@email.com' },
+    { name: 'George Harris', password: bcrypt.hash('georgeharris', 10), email: 'georgeh@email.com' },
   ]);
 }
