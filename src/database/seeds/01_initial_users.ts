c13eac78f21c7c4cbc98b7d0c668ada60525883c
Fabricio Borgobello <fborgobello@gmail.com>
2023-11-28 16:40:32 +0100

    Improve data seeds

--- src/database/seeds/01_initial_users.ts
+++ src/database/seeds/01_initial_users.ts
@@ -7,15 +7,15 @@ export async function seed(knex: Knex): Promise<void> {
 
   // Inserts seed entries
   await knex('users').insert([
-    { name: 'John Doe', password: bcrypt.hash('johndoe', 10), email: 'johndoe@email.com' },
-    { name: 'Jane Doe', password: bcrypt.hash('janedoe', 10), email: 'janedoe@email.com' },
-    { name: 'John Smith', password: bcrypt.hash('johnsmith', 10), email: 'johnsmith@email.com' },
-    { name: 'Alice Johnson', password: bcrypt.hash('alicejohnson', 10), email: 'alicej@email.com' },
-    { name: 'Bob Brown', password: bcrypt.hash('bobbrown', 10), email: 'bobb@email.com' },
-    { name: 'Charlie Davis', password: bcrypt.hash('charliedavis', 10), email: 'charlied@email.com' },
-    { name: 'Diana Evans', password: bcrypt.hash('dianaevans', 10), email: 'dianae@email.com' },
-    { name: 'Ethan Fox', password: bcrypt.hash('ethanfox', 10), email: 'ethanf@email.com' },
-    { name: 'Fiona Green', password: bcrypt.hash('fionagreen', 10), email: 'fionag@email.com' },
-    { name: 'George Harris', password: bcrypt.hash('georgeharris', 10), email: 'georgeh@email.com' },
+    { name: 'John Doe', password: await bcrypt.hash('johndoe', 10), email: 'johndoe@email.com' },
+    { name: 'Jane Doe', password: await bcrypt.hash('janedoe', 10), email: 'janedoe@email.com' },
+    { name: 'John Smith', password: await bcrypt.hash('johnsmith', 10), email: 'johnsmith@email.com' },
+    { name: 'Alice Johnson', password: await bcrypt.hash('alicejohnson', 10), email: 'alicej@email.com' },
+    { name: 'Bob Brown', password: await bcrypt.hash('bobbrown', 10), email: 'bobb@email.com' },
+    { name: 'Charlie Davis', password: await bcrypt.hash('charliedavis', 10), email: 'charlied@email.com' },
+    { name: 'Diana Evans', password: await bcrypt.hash('dianaevans', 10), email: 'dianae@email.com' },
+    { name: 'Ethan Fox', password: await bcrypt.hash('ethanfox', 10), email: 'ethanf@email.com' },
+    { name: 'Fiona Green', password: await bcrypt.hash('fionagreen', 10), email: 'fionag@email.com' },
+    { name: 'George Harris', password: await bcrypt.hash('georgeharris', 10), email: 'georgeh@email.com' },
   ]);
 }
