7db984c511cac3312f5947a5c7a0ee9a974f19f4
Fabricio Borgobello <fborgobello@gmail.com>
2023-11-28 18:34:46 +0100

    Add bcrypt to external

--- esbuild.config.js
+++ esbuild.config.js
@@ -10,7 +10,7 @@ build({
   target: 'es2020',
   minify: false,
   sourcemap: true,
-  external: ['express', 'cors', 'dotenv', 'helmet', 'morgan', 'pg'],
+  external: ['express', 'cors', 'dotenv', 'helmet', 'morgan', 'pg', 'bcrypt'],
   loader: {
     '.json': 'json',
   },
