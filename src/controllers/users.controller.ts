cb826da2407b0ccfdd90fa2d02141f439b804ff6
Fabricio Borgobello <fborgobello@gmail.com>
2023-11-28 15:20:23 +0100

    Add initial controllers

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,68 +1,37 @@
+import { db } from '@/database';
 import { Request, Response } from 'express';
-import { type User } from '@/models/users.model';
-
-let users: User[] = [
-  {
-    id: 1,
-    name: 'John Doe',
-    email: 'johndoe@email.com',
-    password: 'password',
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  },
-  {
-    id: 2,
-    name: 'Jane Doe',
-    email: 'janedoe@email.com',
-    password: 'password',
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  },
-];
 
 // List
-export const getUsers = (_req: Request, res: Response) => {
-  res.status(200).json(users);
+export const getUsers = async (_req: Request, res: Response) => {
+  const users = await db('users').select('*');
+  res.json(users);
 };
 
 // Retrieve
-export const getUserById = (req: Request, res: Response) => {
-  const userId = Number(req.params.id);
-  const foundUser = users.find((user) => user.id === userId);
-  if (foundUser) {
-    res.status(200).json(foundUser);
-  } else {
-    res.status(404).json({ message: 'User not found' });
-  }
+export const getUserById = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const user = await db('users').where({ id }).first();
+  res.json(user);
 };
 
 // Create
-export const createUser = (req: Request, res: Response) => {
-  const newUser: User = req.body;
-  users.push(newUser);
-  res.status(201).json({ message: 'User added', user: newUser });
+export const createUser = async (req: Request, res: Response) => {
+  const { name, email, password } = req.body;
+  const user = await db('users').insert({ name, email, password }).returning('*');
+  res.json(user);
 };
 
 // Update
-export const updateUser = (req: Request, res: Response) => {
-  const userId = Number(req.params.id);
-  const userIndex = users.findIndex((user) => user.id === userId);
-  if (userIndex > -1) {
-    users[userIndex] = { ...users[userIndex], ...req.body };
-    res.status(200).json({ message: 'User updated', user: users[userIndex] });
-  } else {
-    res.status(404).json({ message: 'User not found' });
-  }
+export const updateUser = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { name, email, password } = req.body;
+  const user = await db('users').where({ id }).update({ name, email, password }).returning('*');
+  res.json(user);
 };
 
 // Delete
-export const deleteUser = (req: Request, res: Response) => {
-  const userId = Number(req.params.id);
-  const newUserList = users.filter((user) => user.id !== userId);
-  if (newUserList.length !== users.length) {
-    users = newUserList;
-    res.status(200).json({ message: 'User deleted' });
-  } else {
-    res.status(404).json({ message: 'User not found' });
-  }
+export const deleteUser = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  await db('users').where({ id }).delete();
+  res.status(204).send();
 };
--- src/database/seeds/01_initial_users.ts
+++ src/database/seeds/01_initial_users.ts
@@ -6,15 +6,15 @@ export async function seed(knex: Knex): Promise<void> {
 
   // Inserts seed entries
   await knex('users').insert([
-    { id: 1, name: 'John Doe', password: 'johndoe', email: 'johndoe@email.com' },
-    { id: 2, name: 'Jane Doe', password: 'janedoe', email: 'janedoe@email.com' },
-    { id: 3, name: 'John Smith', password: 'johnsmith', email: 'johnsmith@email.com' },
-    { id: 4, name: 'Alice Johnson', password: 'alicejohnson', email: 'alicej@email.com' },
-    { id: 5, name: 'Bob Brown', password: 'bobbrown', email: 'bobb@email.com' },
-    { id: 6, name: 'Charlie Davis', password: 'charliedavis', email: 'charlied@email.com' },
-    { id: 7, name: 'Diana Evans', password: 'dianaevans', email: 'dianae@email.com' },
-    { id: 8, name: 'Ethan Fox', password: 'ethanfox', email: 'ethanf@email.com' },
-    { id: 9, name: 'Fiona Green', password: 'fionagreen', email: 'fionag@email.com' },
-    { id: 10, name: 'George Harris', password: 'georgeharris', email: 'georgeh@email.com' },
+    { name: 'John Doe', password: 'johndoe', email: 'johndoe@email.com' },
+    { name: 'Jane Doe', password: 'janedoe', email: 'janedoe@email.com' },
+    { name: 'John Smith', password: 'johnsmith', email: 'johnsmith@email.com' },
+    { name: 'Alice Johnson', password: 'alicejohnson', email: 'alicej@email.com' },
+    { name: 'Bob Brown', password: 'bobbrown', email: 'bobb@email.com' },
+    { name: 'Charlie Davis', password: 'charliedavis', email: 'charlied@email.com' },
+    { name: 'Diana Evans', password: 'dianaevans', email: 'dianae@email.com' },
+    { name: 'Ethan Fox', password: 'ethanfox', email: 'ethanf@email.com' },
+    { name: 'Fiona Green', password: 'fionagreen', email: 'fionag@email.com' },
+    { name: 'George Harris', password: 'georgeharris', email: 'georgeh@email.com' },
   ]);
 }
